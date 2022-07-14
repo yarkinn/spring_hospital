67ad5df90403350282497473ba55992ea97a0842
yarkinn <82332307+yarkinn@users.noreply.github.com>
2022-07-14 17:56:01 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_hospital/SpringHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHospitalApplication.class, args);
+	}
+
+}
