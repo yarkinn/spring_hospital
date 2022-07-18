d1fcaa86a3d306ce82d99efc905f52a197824f6d
yarkinn <82332307+yarkinn@users.noreply.github.com>
2022-07-18 17:25:49 +0300

    oldu mu 2

--- src/main/java/com/example/spring_hospital/dao/EmployeeRepository.java
+++ src/main/java/com/example/spring_hospital/dao/EmployeeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+@CrossOrigin//("http://localhost:4200")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
 }
